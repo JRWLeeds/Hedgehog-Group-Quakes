4c42a5a1f7230e73066a9a94d467dcfc246be11c
josh <ee11j2w@leeds.ac.uk>
2016-01-20 14:51:04 +0000

    Sped up for debugging purposes

--- map_quakes.py
+++ map_quakes.py
@@ -6,12 +6,15 @@ from import_quakes import csv_loader
 print('Magnitude options: all, 1.0, 2.5, 4.5, significant')
 print('(Magnitude values are greater than or equal to ranges)')
 print('Time options: hour, day, week, month')
-whichset = input('Do you want a specific dataset, or access to all of them? (answer spec/all): ')
-
+#whichset = input('Do you want a specific dataset, or access to all of them? (answer spec/all): ')
+whichset = 'spec'
 #If one specific dataset
 if whichset == 'spec':
-    specmag = input('Please input magnitude from options above: ')
-    spectime = input('Please input time from options above: ')
+    #specmag = input('Please input magnitude from options above: ')
+    #spectime = input('Please input time from options above: ')
+	specmag = '4.5'
+	spectime = 'hour'
+	
 	##Call function for downloading data here, followed by plotting
     dataset = csv_loader(specmag,spectime)
     print(dataset[0:3])
