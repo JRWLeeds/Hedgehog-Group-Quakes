0b70fa96fdcca616a0e10ffbd6ed4a036d81982a
josh <ee11j2w@leeds.ac.uk>
2016-01-20 14:40:20 +0000

    Changed tab and fixed some errors

---
+++ map_quakes.py
@@ -0,0 +1,20 @@
+#Importing initial functions
+from import_quakes import csv_loader
+##Importing plotting function
+
+#Asking whether the user wants one data set or access to all
+print('Magnitude options: all, 1.0, 2.5, 4.5, significant')
+print('(Magnitude values are greater than or equal to ranges)')
+print('Time options: hour, day, week, month')
+whichset = input('Do you want a specific dataset, or access to all of them? (answer spec/all): ')
+
+#If one specific dataset
+if whichset == 'spec':
+    specmag = input('Please input magnitude from options above: ')
+    spectime = input('Please input time from options above: ')
+	##Call function for downloading data here, followed by plotting
+    dataset = csv_loader(specmag,spectime)
+    print(dataset[0:3])
+	
+#else
+    ##Call function in loop to download all data, then call to plot 
