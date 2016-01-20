672c40db6cc2d2e35235a5596c6120a536ac011b
jennie <eejed@leeds.ac.uk>
2016-01-20 16:25:08 +0000

    Different sized magnitude markers

--- plot_quakes.py
+++ plot_quakes.py
@@ -5,11 +5,11 @@ import csv
 # Open the earthquake data file.
 filename = 'datasets/earthquake_data.csv'
 
-# Create empty lists for the latitudes and longitudes.
-lats, lons = [], []
+# Create empty lists for the latitudes, longitudes and magnitudes.
+lats, lons, mag = [], [], []
 
 # Read through the entire file, skip the first line,
-#  and pull out just the lats and lons.
+#  and pull out just the lats, lons and mag.
 with open(filename) as f:
     # Create a csv reader object.
     reader = csv.reader(f)
@@ -17,10 +17,11 @@ with open(filename) as f:
     # Ignore the header row.
     next(reader)
     
-    # Store the latitudes and longitudes in the appropriate lists.
+    # Store the latitudes, longitudes and magnitudes in the appropriate lists.
     for row in reader:
         lats.append(float(row[1]))
         lons.append(float(row[2]))
+        mag.append(float(row[4]))
  
 # --- Build Map ---
 from mpl_toolkits.basemap import Basemap
@@ -37,6 +38,18 @@ eq_map.drawmeridians(np.arange(0, 360, 30))
 eq_map.drawparallels(np.arange(-90, 90, 30))
  
 x,y = eq_map(lons, lats)
-eq_map.plot(x, y, 'ro', markersize=6)
- 
+i=0
+
+# Plot the data with different size markers for earthquakes of different magnitudes
+for magnitude in mag:
+    if magnitude < 3.0:
+        eq_map.plot(x[i], y[i], 'go', markersize=4) # plots magnitude up to 3
+    elif magnitude < 5.0:
+        eq_map.plot(x[i], y[i], 'yo', markersize=6) # plots magnitude 3 and 4
+    elif magnitude < 6.5:
+        eq_map.plot(x[i], y[i], 'ro', markersize=8) # plots magnitude 5-6.5
+    else:
+        eq_map.plot(x[i], y[i], 'bo', markersize=10) # plots magnitude 6.5 and higher
+    i=i+1
+
 plt.show()
