49593b0ae38a5b9b66fa737147b7ab256160511f
Charlie <earcdu@leeds.ac.uk>
2016-01-20 14:44:05 +0000

    3rd attempt at URL downloading function

--- import_quakes.py
+++ import_quakes.py
@@ -5,7 +5,7 @@ def csv_loader(mag,time):
     import urllib 
     urlbase = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/'
     url= urlbase+mag+'_'+time+'.csv' 
-    response = urllib.urlopen(url)
+    response = urllib.request.urlopen(url)
     cr = csv.reader(response)
 
 
