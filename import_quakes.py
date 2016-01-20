c7d644dc3aeda8395fc8528046488961a7ac979d
Charlie <earcdu@leeds.ac.uk>
2016-01-20 14:54:49 +0000

    Blah blah blah need words

--- import_quakes.py
+++ import_quakes.py
@@ -2,7 +2,7 @@
 
 def csv_loader(mag,time):
     import csv
-    import urllib 
+    import urllib.request 
     urlbase = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/'
     url= urlbase+mag+'_'+time+'.csv' 
     response = urllib.request.urlopen(url)
